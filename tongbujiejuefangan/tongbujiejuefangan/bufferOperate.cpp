b5937538ded2da3c3da745c03e8b6a804dd75cba
wutingkang <wutingkang1@gmail.com>
2016-12-02 17:24:31 +0800

    第二次

--- tongbujiejuefangan/tongbujiejuefangan/bufferOperate.cpp
+++ tongbujiejuefangan/tongbujiejuefangan/bufferOperate.cpp
@@ -1,13 +1,27 @@
-#include"stdafx.h"
+#include "stdafx.h"
 #include "buffer.h"
 
 buffer_item buffer[BUFFER_SIZE];
+int currentPosition = 0;
 
 //成功 0；失败 - 1
 int insert_item(buffer_item item) {
-
+	if (BUFFER_SIZE > currentPosition && 0 <= currentPosition) {
+		buffer[currentPosition] = item;
+		currentPosition++;
+		return 0;
+	}
 	
+	return -1;
 }
-int remote_item(buffer_item item) {
-	
+
+//先按照堆栈的方式remote
+int remote_item(buffer_item *item) {
+	if (BUFFER_SIZE > currentPosition && 0 < currentPosition) {
+		buffer[currentPosition] = *item;
+		currentPosition--;
+		return 0;
+	}
+
+	return -1;
 }
--- tongbujiejuefangan/tongbujiejuefangan/tongbujiejuefangan.cpp
+++ tongbujiejuefangan/tongbujiejuefangan/tongbujiejuefangan.cpp
@@ -2,7 +2,6 @@
 //
 
 #include "stdafx.h"
-
 #include "buffer.h"
 
 using namespace std; 
@@ -10,21 +9,32 @@ using namespace std;
 HANDLE full, empty;
 HANDLE mutex = CreateMutex(NULL, FALSE, NULL);
 
+//Ïë°ì·¨£º #include "bufferOperate.cpp"
+extern int currentPosition;
+int insert_item(buffer_item item);
+int remote_item(buffer_item *item);
+
+//do what?
 int init()
 {
+	currentPosition = 0;
 	return 0;
 }
 
 
+
 int main(int argc, char *argv[]) {
-	/*Get command line arguments argc[1..2..3]
+	/*Get command line arguments argc[1..2..3]*/
+
+	/*
 		2.intialize buffer
 		3.create producer thread
 		4.create consumer thread
 		5.sleep
 		6.exit*/
 	init();
+	
 
-	printf("hello\n %d", BUFFER_SIZE);
+	printf("hello\n %d", currentPosition);
 	return 0;
 }
